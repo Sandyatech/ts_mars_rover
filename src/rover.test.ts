35fc93caf6a786d3adb304a5d3cb0d2711e2c8db
sandyatech <sandhya.mskgmail.com>
2023-02-14 11:36:28 +0000

    added more negative tests

--- src/rover.test.ts
+++ src/rover.test.ts
@@ -9,7 +9,7 @@ describe('Test Rover object type', function () {
         assert.strictEqual(typeof rover === 'function', true)
     });
 
-    it('Test Rover cannot move', function () {
+    it('Test Rover cannot move with out of Boundary Instruction', function () {
 
         //@todo this will need a proper parser solution
         let command = "5 5\n" + //Size of plateue.
@@ -21,49 +21,54 @@ describe('Test Rover object type', function () {
 
     });
 
-    it('Test Rover move', function () {
-        
+    it('Test Rover cannot move with Improper Left,Right,Move instruction', function () {
+
         //@todo this will need a proper parser solution
         let command = "5 5\n" + //Size of plateue.
             "1 2 N\n" +         //Start position
-            "LMLMLMLMM"; 
+            "LMLMLMLMK";
+        assert.strictEqual('Improper Left,Right,Move instruction', rover(command));
+
+    });
+
+    it('Test Rover cannot move with Improper direction instruction', function () {
+
+        //@todo this will need a proper parser solution
+        let command = "5 5\n" + //Size of plateue.
+            "1 2 K\n" +         //Start position
+            "LMLMLMLM";
+        assert.strictEqual('Improper direction instruction', rover(command));
+
+    });
+
+    it('Test Rover move', function () {
+
+        //@todo this will need a proper parser solution
+        let command = "5 5\n" + //Size of plateue.
+            "1 2 N\n" +         //Start position
+            "LMLMLMLMM";
         assert.strictEqual('1 3 N', rover(command));
 
     });
 
+    it('Test Rover move with kata instruction', function () {
+
+        
+        let command = "5 5\n" +
+
+        "1 2 N\n" +
+
+        "LMLMLMLMM\n" +
+
+        "3 3 E\n" +
+
+        "MMRMMRMRRM";
+        assert.strictEqual('1 3 N\n5 1 E', rover(command));
+
+    });
+
+
+
 })
-        /*let testCommands = {
-            'plateau_size': {
-                'width': 5,
-                'height': 5
-            },
-            'starting_position': {
-                'x': 1,
-                'y': 2,
-                'd': 'N'
-            },
-            'moving_command': 'LMLMLMLMM'
-        }
-
-        assert.strictEqual('1 3 N', R1.execute(testCommands));
-
-
-        let R2 = new Rover();
-
-        let testCommands2 = {
-            'plateau_size': {
-                'width': 5,
-                'height': 5
-            },
-            'starting_position': {
-                'x': 3,
-                'y': 3,
-                'd': 'E'
-            },
-            'moving_command': 'MMRMMRMRRM'
-        }
-
-        assert.strictEqual('5 1 E', R2.execute(testCommands2)); */
-
-
+      
 
